b7580e9c623c7326a14233e2e3cf5208d923f9a7
Theodore Maxwell Thomas <t_max_thomas@hotmail.com>
2016-12-30 14:06:02 -0500

    Split file
    Split the PID controller code into three separate files to better
    conform with C++ standards

---
+++ PID_controller/PID.h
@@ -0,0 +1,17 @@
+#ifndef PID_H_
+#define PID_H_
+
+class PID{
+public:
+  PID(float p_ = 0, float i_ = 0, float d_ = 0) : p(p_), i(i_), d(d_), set_pt(0), old_input(0), total_err(0) {}
+  void update();
+  float p, i, d;
+  float set_pt, input, old_input, err, total_err, d_input;
+  PID* next;
+};
+
+void serialWrite(float val);
+float serialRead();
+void serialRead(byte& val);
+
+#endif
