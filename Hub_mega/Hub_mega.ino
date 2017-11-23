a25f2a0f9c02cb9f2ec7a14bd20f80fd342cdba0
tmaxthomas <t_max_thomas@hotmail.com>
2017-11-23 13:36:30 -0500

    Removed PID co-processor support from codebase

--- Hub_mega/Hub_mega.ino
+++ Hub_mega/Hub_mega.ino
@@ -10,43 +10,53 @@
                            Comp ## _pos += delta
 
 
-//Still need to implement register trickery
+//Sizes for outgoing and incoming packets. Subject to change once we actually figure out what we're sending.
+#define OUT_PACKET_SIZE 32
+#define IN_PACKET_SIZE 32
 
 int spool_pos, shaft_pos, ballast_pos;
 byte spool_old, shaft_old, ballast_old;
 bool spool_dir, ballast_dir, shaft_dir;
 float shaft_speed;
 
+//PID struct
+struct PID {
+  float p, i, d;
+};
+
 int old_mils;
 void setup() {
   Serial.begin(115200);     //USB
-  Serial1.begin(115200);    //PID controller
-  Serial2.begin(115200);    //Radio/mini-sub
-  Serial3.begin(115200);    
-  //Put p, i, and d values for controllers in these arrays
-  float p_vals[NUM_PID] = { 0.01 };
-  float i_vals[NUM_PID] = { 0 };
-  float d_vals[NUM_PID] = { 0 };
-  //Then write the arrays (assuming writing to Serial1)
-  for(int a = 0; a < NUM_PID; a++){
-    serialWrite(p_vals[a], 1);
-    serialWrite(i_vals[a], 1);
-    serialWrite(d_vals[a], 1);
-  }
-  //Write signal-termination bytes
-  serialWrite(0, 1);
-  old_mils = millis(); 
+  Serial1.begin(115200);    //Backup?
+  Serial2.begin(115200);    //Radio/mini-sub    
 
-  //Register config-set them all to output
+  struct PID ctrl[3];
+  ctrl[0] = { .p = 0.01, .i = 0, .d = 0 };
+  
+  //Register config
   DDRC = 0b00000000;
   DDRA = 0b00000000;
   DDRL = 0b00000000;
+
+  old_mils = millis(); 
 }
 
 void loop() {
-  //Recieve packet from groundstation
+  byte checksum = 0;
+  //Recieve packet from groundstation (TODO)
+  byte in_packet[IN_PACKET_SIZE];
+  Serial.readBytes(in_packet, IN_PACKET_SIZE);
   
-  
+  //Confirm packet integrity
+  for(int i = 0; i < IN_PACKET_SIZE; i++) 
+    checksum ^= in_packet[i];
+
+  if(!checksum) {
+    /*
+     * RETRIEVE DATA FROM PACKET HERE
+     */
+  }
+      
   //Read from sensors (TODO)
 
   //Reading from encoder Arduinos
@@ -62,10 +72,20 @@ void loop() {
   
   //Read/write from/to PID arduino (TODO)
   
-
-  //Send data packet to groundstation/minisub
-  byte packet[32];
   
+  //Send data packet to groundstation/minisub
+  byte out_packet[OUT_PACKET_SIZE];
+  /*
+   * POPULATE PACKET HERE
+   */
+
+  //Compute and append checksum
+  checksum = 0;
+  for(int i = 0; i < OUT_PACKET_SIZE - 1; i++)
+    checksum ^= out_packet[i];
+  out_packet[OUT_PACKET_SIZE - 1] = checksum;
+  
+  Serial.write(out_packet, OUT_PACKET_SIZE);
   
   while(millis() - old_mils < D_MILS);
 }
