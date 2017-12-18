1bf8f45319fc2ba4f27cc17c287d90ea80c08d05
tmaxthomas <t_max_thomas@hotmail.com>
2017-12-18 13:38:42 -0500

    ReadReg update to account for direction

--- Hub_mega/Hub_mega.ino
+++ Hub_mega/Hub_mega.ino
@@ -5,7 +5,7 @@
 #define ReadReg(Comp, Reg) byte Comp ## _new = Reg; \
                            delta = Comp ## _new - Comp ## _old; \
                            Comp ## _old = Comp ## _new; \
-                           Comp ## _pos += delta
+                           Comp ## _pos += ( Comp ## _dir ? delta : -delta )
 
 
 //Sizes for outgoing and incoming packets. Subject to change once we actually figure out what we're sending.
@@ -32,38 +32,38 @@ float updatePID(struct PID ctrl, float set_pt, float val) {
   ctrl.total_err += err;
   float d_val = ctrl.old_val - val;
   ctrl.old_val = val;
-  return (err * ctrl.p) + (ctrl.total_err * ctrl.i) - (d_val * ctrl.d); 
+  return (err * ctrl.p) + (ctrl.total_err * ctrl.i) - (d_val * ctrl.d);
 }
 
 int old_mils;
 void setup() {
   Serial.begin(BAUD_RATE);     //USB
   Serial1.begin(BAUD_RATE);    //Backup?
-  Serial2.begin(BAUD_RATE);    //Radio/mini-sub    
+  Serial2.begin(BAUD_RATE);    //Radio/mini-sub
 
   struct PID ctrl[3];
   ctrl[0] = { .p = 0.01, .i = 0, .d = 0 };
   ctrl[1] = { .p = 0.01, .i = 0, .d = 0 };
   ctrl[2] = { .p = 0.01, .i = 0, .d = 0 };
-  
+
   //Register config
   DDRC = 0b00000000;
   DDRA = 0b00000000;
   DDRL = 0b00000000;
 
-  old_mils = millis(); 
+  old_mils = millis();
 }
 
 void loop() {
   //May want to reconsider how we pass data, accounting for how damn long it takes
-  
+
   byte checksum = 0;
   //Recieve packet from groundstation
   byte in_packet[IN_PACKET_SIZE];
   Serial.readBytes(in_packet, IN_PACKET_SIZE);
-  
+
   //Confirm packet integrity
-  for(int i = 0; i < IN_PACKET_SIZE; i++) 
+  for(int i = 0; i < IN_PACKET_SIZE; i++)
     checksum ^= in_packet[i];
 
   if(!checksum) {
@@ -71,7 +71,7 @@ void loop() {
      * TODO: RETRIEVE DATA FROM PACKET
      */
   }
-      
+
   //TODO: Read from sensors
 
   //Reading from encoder Arduinos
@@ -79,14 +79,14 @@ void loop() {
   //Pins 37 (PORTC 0) to 30 (PORTC 7) - Remember to plug this one in backwards
   ReadReg(ballast, PORTC);
 
-  //Pins 22 (PORTA 0) to 29 (PORTA 7) 
+  //Pins 22 (PORTA 0) to 29 (PORTA 7)
   ReadReg(spool, PORTA);
 
   //Pins 49 (PORTL 0) to 42 (PORTL 7) - This one goes in backwards as well
   ReadReg(shaft, PORTL);
-  
+
   //TODO: Update PID controllers and write new vals to motor controllers
-  
+
   //TODO: Send data packet to groundstation/minisub
   byte out_packet[OUT_PACKET_SIZE];
   /*
@@ -98,9 +98,9 @@ void loop() {
   for(int i = 0; i < OUT_PACKET_SIZE - 1; i++)
     checksum ^= out_packet[i];
   out_packet[OUT_PACKET_SIZE - 1] = checksum;
-  
+
   Serial.write(out_packet, OUT_PACKET_SIZE);
-  
+
   while(millis() - old_mils < D_MILS);
   old_mils = millis();
 }
