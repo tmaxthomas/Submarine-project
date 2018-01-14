ff6b118aa67eecd1f289b2585630b58c4e96df98
Theodore Maxwell Thomas <t_max_thomas@hotmail.com>
2018-01-14 15:24:31 -0500

    Added output packet assembly code

--- Hub_mega/Hub_mega.ino
+++ Hub_mega/Hub_mega.ino
@@ -10,7 +10,7 @@
                            Comp ## _old = Comp ## _new; \
                            Comp ## _pos += ( Comp ## _dir ? delta : -delta )
 
-#define PWMwrite(addr, val) Wire.beginTransmission(PWM_ADDR); \
+#define PWMWrite(addr, val) Wire.beginTransmission(PWM_ADDR); \
                             Wire.write(addr); \
                             Wire.write(val); \
                             Wire.endTransmission()
@@ -20,6 +20,7 @@
 #define OUT_PACKET_SIZE 9
 #define IN_PACKET_SIZE 9
 
+// I2C address of the PWM controller
 #define PWM_ADDR 0x40
 
 uint8_t in_pack_buf[IN_PACKET_SIZE], out_pack_buf[OUT_PACKET_SIZE];
@@ -27,29 +28,30 @@ uint8_t in_pack_buf[IN_PACKET_SIZE], out_pack_buf[OUT_PACKET_SIZE];
 //Encoder data
 uint16_t spool_pos, shaft_pos, ballast_pos;
 uint16_t spool_set, ballast_set;
-int16_t shaft_voltage;
+int8_t shaft_voltage;
 uint8_t spool_old, shaft_old, ballast_old;
 bool spool_dir, ballast_dir, shaft_dir;
 float shaft_speed;
+uint8_t flooded;
 
 
 //Other sensor data
 
 //PID struct
 struct PID {
-  float p, i, d;
-  float total_err, old_val;
+    float p, i, d;
+    float total_err, old_val;
 } ctrl[2];
 
 
 
 //Updates PID controller and returns updated motor power percentage
 float updatePID(struct PID c, float set_pt, float val) {
-  float err = set_pt - val;
-  c.total_err += err;
-  float d_val = c.old_val - val;
-  c.old_val = val;
-  return (err * c.p) + (c.total_err * c.i) - (d_val * c.d);
+    float err = set_pt - val;
+    c.total_err += err;
+    float d_val = c.old_val - val;
+    c.old_val = val;
+    return (err * c.p) + (c.total_err * c.i) - (d_val * c.d);
 }
 
 // Runs initialization code for the PWM controller
@@ -65,80 +67,93 @@ void pwm_controller_init() {
 }
 
 void pwm_update(uint8_t num, uint16_t val) {
-    //I really don't trust Arduino's built-in min function
     val = (val < 4095) ? val : 4096;
 
     //Write a whole bunch of magic
     //Refer to the PCA9685 datasheet for more information
     Wire.beginTransmission(PWM_ADDR);
-    uint8_t data = {0x06 + 4*num, 0, 0, val, val>>8};
+    uint8_t data[5] = {0x06 + 4*num, 0, 0, val, val>>8};
     Wire.write(data, 5);
     Wire.endTransmission();
+}
+
+//Emergency abort routine
+//TODO: implement
+void ABORT() {
+
 }
 
 uint32_t old_mils;
 
 void setup() {
-  Serial.begin(BAUD_RATE);     //USB
-  Serial1.begin(BAUD_RATE);    //Radio/mini-sub
+    Serial.begin(BAUD_RATE);     //USB
+    Serial1.begin(BAUD_RATE);    //Radio/mini-sub
 
-  ctrl[0] = { .p = 0.01, .i = 0, .d = 0 };
-  ctrl[1] = { .p = 0.01, .i = 0, .d = 0 };
+    ctrl[0] = { .p = 0.01, .i = 0, .d = 0 };
+    ctrl[1] = { .p = 0.01, .i = 0, .d = 0 };
 
-  //Register config
-  DDRC = 0b00000000;
-  DDRA = 0b00000000;
-  DDRL = 0b00000000;
+    //Register config
+    DDRA &= 0b00000000;
+    DDRC &= 0b00000000;
+    DDRG &= 0b00000111;
+    DDRL &= 0b00000000;
 
-  old_mils = millis();
+    old_mils = millis();
 }
 
 void loop() {
-  byte checksum = 0;
+    byte checksum = 0;
 
-  //Recieve packet from groundstation
-  Serial.readBytes(in_pack_buf, IN_PACKET_SIZE);
+    //Recieve packet from groundstation
+    Serial.readBytes(in_pack_buf, IN_PACKET_SIZE);
 
-  //Confirm packet integrity
-  for(int i = 0; i < IN_PACKET_SIZE; i++)
-    checksum ^= in_pack_buf[i];
+    //Confirm packet integrity
+    for(int i = 0; i < IN_PACKET_SIZE; i++)
+        checksum ^= in_pack_buf[i];
 
-  if(!checksum) {
-    shaft_voltage = in_pack_buf[0] * 4;
-    ballast_set = *(uint16_t*)(in_pack_buf + 4);
-    spool_set = *(uint16_t*)(in_pack_buf + 6);
-  }
+    if(!checksum) {
+        shaft_voltage = in_pack_buf[0];
+        ballast_set = *(uint16_t *) (in_pack_buf + 4);
+        spool_set = *(uint16_t *) (in_pack_buf + 6);
+    }
 
-  //TODO: Read from sensors
+    //Read flooding data and act accordingly
+    flooded = PORTG;
+    if(flooded) ABORT();
 
-  //Reading from encoder Arduinos
-  byte delta;
-  //Pins 37 (PORTC 0) to 30 (PORTC 7) - Remember to plug this one in backwards
-  ReadReg(ballast, PORTC);
+    //Reading from encoder Arduinos
+    byte delta;
+    //Pins 37 (PORTC 0) to 30 (PORTC 7) - Remember to plug this one in backwards
+    ReadReg(ballast, PORTC);
 
-  //Pins 22 (PORTA 0) to 29 (PORTA 7)
-  ReadReg(spool, PORTA);
+    //Pins 22 (PORTA 0) to 29 (PORTA 7)
+    ReadReg(spool, PORTA);
 
-  //Pins 49 (PORTL 0) to 42 (PORTL 7) - This one goes in backwards as well
-  ReadReg(shaft, PORTL);
+    //Pins 49 (PORTL 0) to 42 (PORTL 7) - This one goes in backwards as well
+    ReadReg(shaft, PORTL);
 
-  //TODO: Update PID controllers and write new vals to motor controllers
+    //TODO: Update PID controllers and write new vals to motor controllers
 
-  //TODO: Send data packet to groundstation/minisub
-  byte out_packet[OUT_PACKET_SIZE];
-  /*
-   * TODO: POPULATE PACKET
-   */
+    byte out_pack_buf[OUT_PACKET_SIZE];
 
-  //Compute and append checksum
-  checksum = 0;
-  for(uint8_t i = 0; i < OUT_PACKET_SIZE - 1; i++)
-    checksum ^= out_pack_buf[i];
+    //Populate packet with data
+    *(float *) out_pack_buf = shaft_speed;
+    *(uint16_t *) (out_pack_buf + 2) = ballast_pos;
+    *(uint16_t *) (out_pack_buf + 4) = spool_pos;
+    out_pack_buf[6] = flooded;
 
-  out_pack_buf[OUT_PACKET_SIZE - 1] = checksum;
+    //Compute and append checksum
+    checksum = 0;
+    for(uint8_t i = 0; i < OUT_PACKET_SIZE - 2; i++)
+        checksum ^= out_pack_buf[i];
+    out_pack_buf[OUT_PACKET_SIZE - 2] = checksum;
 
-  Serial.write(out_pack_buf, OUT_PACKET_SIZE);
+    //Send main sub shaft voltage to mini-sub
+    //TODO: determine conversion formula from main shaft voltage to mini-sub shaft voltage
+    out_pack_buf[OUT_PACKET_SIZE - 1] = shaft_voltage;
 
-  while(millis() - old_mils < D_MILS);
-  old_mils = millis();
+    Serial.write(out_pack_buf, OUT_PACKET_SIZE);
+
+    while(millis() - old_mils < D_MILS);
+        old_mils = millis();
 }
