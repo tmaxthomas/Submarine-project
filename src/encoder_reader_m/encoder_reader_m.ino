8558f55f66b16ed76542ffc834bdefb8f2a039bd
tmaxthomas <t_max_thomas@hotmail.com>
2018-08-11 15:45:03 -0400

    Wrote multi-encoder Nano code

--- src/encoder_reader_m/encoder_reader_m.ino
+++ src/encoder_reader_m/encoder_reader_m.ino
@@ -1,34 +1,45 @@
 #include <stdint.h>
 
 // Stores the tick counts
-volatile uint8_t tick_count_1;
+volatile uint16_t aggregated_counter;
 
-/*
- * Updates the tick counter when called, then writes the updated tick count to the pins
- * Counter should automatically wrap around to 0
- */
-void updateCount(){
-    tick_count++;
-    PORTD = tick_count << 4;
-    PORTB = tick_count >> 4;
+void update_ports() {
+    PORTD = (uint8_t) (aggregated_counter << 4);
+    PORTB = (uint8_t) (aggregated_counter >> 4);
+
+}
+
+void update_count_2(){
+    uint8_t tick_count = (aggregated_counter & 0b111) + 1;
+    aggregated_counter = (aggregated_counter & 0b111111000) | tick_count;
+    update_ports();
+}
+
+void update_count_3(){
+    uint8_t tick_count = (aggregated_counter & 0b111000) + 0b1000;
+    aggregated_counter = (aggregated_counter & 0b111000111) | tick_count;
+    update_ports();
 }
 
 
-//pin that the phototransistor circuit is connected to
-const int photoPin = 3;
+void update_count_4(){
+    uint8_t tick_count = (aggregated_counter & 0b111000000) + 0b1000000;
+    aggregated_counter = (aggregated_counter & 0b111111) | tick_count;
+    update_ports();
+}
 
 void setup() {
     // Configure output pins
     DDRD |= 0b11110000;
-    DDRB |= 0b1111;
-    tick_count = 0;
-    // Create an interrupt routine using a DI pin, ISR, and a positive edge trigger
-    attachInterrupt(digitalPinToInterrupt(photoPin), updateCount, RISING);
+    DDRB |= 0b11111;
+    aggregated_counter = 0;
+
+    attachInterrupt(digitalPinToInterrupt(2), update_count_2, RISING);
+    attachInterrupt(digitalPinToInterrupt(3), updateCount, RISING);
+    attachInterrupt(digitalPinToInterrupt(4), updateCount, RISING);
   
     // Prevent loop() from ever being called, because repeated calls to loop slow things down
     for(;;);
 }
 
 void loop() {}
-
-
